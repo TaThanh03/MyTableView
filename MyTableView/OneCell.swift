a0ed1a36e7e1ce3ac9b50f283fa170277e36f445
TA THANH <ta.thanh.03@gmail.com>
2018-12-08 22:20:00 +0100

    final

---
+++ MyTableView/OneCell.swift
@@ -0,0 +1,18 @@
+//
+//  OneCell.swift
+//  MyTableView
+//
+//  Created by TA Trung Thanh on 08/12/2018.
+//  Copyright © 2018 TA Trung Thanh. All rights reserved.
+//
+
+import UIKit
+
+class OneCell: NSObject {
+    var label = ""
+    var detail = ""
+    init(l: String, d: String) {
+        label = l
+        detail = d
+    }
+}
